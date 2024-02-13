dc4422b08bcebf81f17235eb4c366808f4798e66
HP <deborah.oyinda@yahoo.com>
2024-02-13 21:41:11 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/wtm_savings/wtm_savings_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wtm_savings.wtm_savings_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/account_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AccountView extends StatelessWidget{
+  const AccountView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text("Account Section");
+  }
+}
